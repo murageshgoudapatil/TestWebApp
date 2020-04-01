59f994c8868c0e12b4982ca2b4ecaf3fabe03501
murageshgoudapatil <31233741+murageshgoudapatil@users.noreply.github.com>
2020-04-01 22:11:42 +0530

    Test Update

---
+++ mobile-app-ws/src/main/java/com/patil/app/ws/exceptions/UserServiceException.java
@@ -0,0 +1,15 @@
+package com.patil.app.ws.exceptions;
+
+public class UserServiceException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6814946532492845471L;
+
+	public UserServiceException(String message) {
+		super(message);
+		
+	}
+}
+	
---
+++ mobile-app-ws/src/main/java/com/patil/app/ws/userservice/UserService.java
@@ -0,0 +1,8 @@
+package com.patil.app.ws.userservice;
+
+import com.patil.app.ws.ui.model.response.UserDetailsRequestModel;
+import com.patil.app.ws.ui.model.response.UserRest;
+
+public interface UserService {
+	UserRest createUser(UserDetailsRequestModel userDetails);
+}
